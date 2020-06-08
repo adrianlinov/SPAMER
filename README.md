849b44c4d4b6945064f00ca146bd7479e0b70987
TutorMate <adrianlino@MacBook-Pro-de-Adrian.local>
2020-06-07 22:14:01 -0500

    FINAL

---
+++ README.md
@@ -0,0 +1,63 @@
+# SPAMER
+Automatic email sender with Gmail IPA and Excel.
+
+## MacOS
+
+### Python
+**Para correr el siguiente programa es necesario usar Python 3.6.**
+En caso de MacOS Python 3.6 ya viene instalado y se ejecuta corriendo el comando en terminal
+``` python3 ```
+
+### Instalar dependencias
+Instalar las dependencias con sudo utilizando el siguiente comando.
+
+```sudo pip3 install --upgrade beautifulSoup4 openpyxl google-api-python-client google-auth-httplib2 google-auth-oauthlib```
+
+
+## Windows
+
+### Python
+**Para correr el siguiente programa es necesario usar Python 3.6.**
+En caso de Windows es necesario instalarlo, para ello ingresar al siguiente link y descargar el archivo correspondiente para tu sistema.
+<https://www.python.org/downloads/release/python-367/>
+
+Tambien se puede incluir Python en las variables de usuario para manejarlo mas facil.
+
+### Instalar dependencias
+Instalar las dependencias ejecutar el siguiente comando.
+
+```pip install --upgrade beautifulSoup4 openpyxl google-api-python-client google-auth-httplib2 google-auth-oauthlib```
+
+--------
+
+## Ejecutar
+
+Ejecutar el archivo main.py con Python 3.6 posterior a descargar las dependencias.
+
+## Archivos
+
+Mantener siempre todos los archivos a utilizar dentro de la carpeta root del programa, desde las plantillas hasta los adjuntos.
+
+## Plantilla Excel
+
+SPAMER utiliza una plantilla en Excel la cual cada fila representa un correo distinto a enviar, de modo que el programa itera por todas las filas utilizando el campo EMAIL como el correo destino y la columna ATTACHMENT para los adjuntos del mismo.
+A la plantilla se le puede añadir columnas, estas columnas deben contar en el encabezado el nombre de una variable, la cual el programa va a buscar dentro de la plantilla HTML y remplazar por el valor de cada fila en su respectivo correo. 
+
+**IMPORTANTE:** Al correr el programa los campos de la plantilla deben estar en VALORES y no en FORMULAS.
+
+## Gmail API Credenciales
+
+Para correr el programa, es necesario vincular el API a tu cuenta de Google como desarrollador. Para ello seguir los siguientes pasos.
+
+1. Ingresar a <https://developers.google.com/gmail/api/quickstart/python>
+2. Hacer click en **Enable the Gmail API**.
+3. Seleccionar la configuración **Desktop**.
+4. Descargar **credentials.json** haciendo click en **Download client configuration**.
+5. Copiar **credentials.json** en el directorio root del programa.
+
+## Plantilla HTML
+
+El programa trabaja con una plantilla HTML la cual puede desarrollar por su cuenta, pero en caso no tenga los conocimientos necesarios puede hacerlo en <https://beefree.io>. Cabe mencionar que **BEEFREE** no sube las imagenes utilizadas en la plantilla a la nube, por lo que al momento de exportar la plantilla es necesario remplazar las direcciones del atributo **src** en las etiquetas **img** por una dirección pública. Esto lo puede hacer subiendo las imagenes a **IMGBB** ingresando al siguiente link <https://imgbb.com>
+
+
+
---
+++ main.py
@@ -0,0 +1,152 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient import errors
+from openpyxl import load_workbook
+from bs4 import BeautifulSoup
+import re
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+import base64
+
+SCOPES = ['https://mail.google.com/']
+
+def main():
+    creds = None
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('gmail', 'v1', credentials=creds)
+    com1 = Comunicador(service)
+    com1.iterarPorDiccionarios()
+
+
+
+class Comunicador:
+    def __init__(self, service):
+        self.service = service
+        self.nombreAMostrar = input("INGRESE NOMBRE A MOSTRAR: ")
+        self.contactos_path = input("INGRESE RUTA DE EXCEL DE CONTACTOS: ")
+        self.contactos = load_workbook(self.contactos_path).active
+        self.copia = input('INGRESE EL CORREO AL CUAL DESEA COPIAR: ')
+        self.asunto = input('INGRESE ASUNTO DEL CORREO: ')
+        self.html_path = input('INGRESE RUTA DEL HTML: ')
+        self.accion = int(input(
+            "INGRESE 1 PARA PRUEBA/ INGRESE 2 PARA ENVIAR/ INGRESE 3 PARA CANCELAR: "))
+
+    def diccionarioFila(self, n):
+        dic = {}
+        for col in self.contactos.columns:
+            if col[0].value != None:
+                dic[str(col[0].value)] = col[n].value
+            else:
+                break
+        return dic
+
+    def iterarPorDiccionarios(self):
+        if self.accion == 1:
+            dic = {}
+            for col in self.contactos.columns:
+                if col[0].value != None:
+                    dic[str(col[0].value)] = col[1].value
+                else:
+                    break
+            editorHTML = EditorHTML(self.html_path, dic)
+            htmlEditado = editorHTML.actualizarHTML()
+            testEmail = input("INGRESE EL CORREO AL CUAL QUIERE ENVIAR LA PRUEBA: ")
+            mensaje = Mensaje(self.service)
+
+            if dic['ATTACHMENT'] == None:
+                pass
+            else:
+                aux = str(dic['ATTACHMENT']).split(';', 50)
+            mensaje.create_message(self.nombreAMostrar, testEmail, self.copia, self.asunto, htmlEditado, aux)
+            mensaje.send_message()
+
+        elif self.accion == 2:
+            for row in range(1, self.contactos.max_row - 1):
+                dic = {}
+                for col in self.contactos.columns:
+                    if col[0].value != None:
+                        dic[str(col[0].value)] = col[row].value
+                    else:
+                        break
+                editorHTML = EditorHTML(self.html_path, dic)
+                htmlEditado = editorHTML.actualizarHTML()
+                mensaje = Mensaje(self.service)
+                print(dic)
+                if dic['ATTACHMENT'] == None:
+                    pass
+                else:
+                    aux = str(dic['ATTACHMENT']).split(';', 50)
+                mensaje.create_message(self.nombreAMostrar, dic['EMAIL'], self.copia, self.asunto, htmlEditado, aux)
+                mensaje.send_message()
+        else:
+            print("CANCELADO SATISFACTORIAMENTE")
+            pass
+
+
+class EditorHTML:
+    def __init__(self, html_path, valores):
+        self.html = BeautifulSoup(open(html_path, 'r').read(), features="html.parser")
+        self.valores = valores
+
+    def actualizarHTML(self):
+        for key in self.valores.keys():
+            target = self.html.find_all(text=re.compile(r'{}'.format(key)))
+            for v in target:
+                if self.valores[key] == None:
+                    v.replace_with(v.replace(key, ""))
+                else:
+                    v.replace_with(v.replace(key, self.valores[key]))
+        return self.html
+
+class Mensaje():
+    def __init__(self, service):
+        self.service = service
+        self.message = None
+
+    def send_message(self):
+        try:
+            message = (self.service.users().messages().send(userId="me", body=self.message)
+                       .execute())
+            print('Message Id: %s' % message['id'])
+            return message
+        except errors.HttpError as error:
+            print('An error occurred: %s' % error)
+
+
+    def create_message(self, sender, to, copia ,subject, html_Editado, atachmentArray):
+        
+        message = MIMEMultipart()
+        message['To'] = to
+        message['From'] = sender
+        message['Subject'] = subject
+        message['CC'] = copia
+        mensajeHTML = MIMEText(html_Editado,'html')
+        message.attach(mensajeHTML)
+
+        for file in atachmentArray:
+            with open(file, "rb") as f:
+                attach = MIMEApplication(f.read(), _subtype="pdf")
+            attach.add_header('Content-Disposition', 'attachment', filename=str(file))
+            message.attach(attach)
+
+        self.message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
+
+if __name__ == '__main__':
+    main()
